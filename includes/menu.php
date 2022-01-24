1ab68f616e0c4a48711503694fe54173a2c06541
mdlsvensson <wilmer.lindau@gmail.com>
2022-01-24 14:47:19 +0100

    menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <nav>
                 <ul>
                     <li><a href="index.php">Att-göra</a></li>
-                    <li><a href="variabler.php">JSON-fil</a></li>
+                    <li><a href="json.php">JSON-fil</a></li>
                 </ul>
             </nav>
