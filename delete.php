859dc2c334c005c31376b2ad40dd52f1ec148058
mdlsvensson <wilmer.lindau@gmail.com>
2022-01-25 16:38:09 +0100

    finished delete.php

--- delete.php
+++ delete.php
@@ -2,14 +2,10 @@
   $json = file_get_contents('todolist.json');
   $todoListData = json_decode($json, true);
   $id = $_POST['id'];
-  foreach ($todoListData as $todo) {
-    $key = array_search($id, $todo);
-    echo '<pre>';
-    var_dump($key);
-    echo '</pre>';
+  foreach ($todoListData as $key => $todo) {
+    if ($todo['id'] == $id) {
+      unset($todoListData[$key]);
+    }
   }
-  echo '<p> seperator </p>';
-  unset($todoListData['id'][$id]);
   file_put_contents('todolist.json', json_encode($todoListData, JSON_PRETTY_PRINT));
-
 ?>
